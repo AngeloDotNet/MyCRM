68f2ed06edabc61e16c60d66bab342255ae52a67
Angelo Pirola <angelo9871@gmail.com>
2026-01-26 00:18:09 +0100

    Add S3 bucket existence extension for IAmazonS3
    Introduced StorageS3Extensions.cs with an extension method EnsureBucketExistsAsync for IAmazonS3 to help ensure S3 buckets exist. Includes commented-out alternative logic and currently uses an async void signature.

---
+++ src/Api/Extensions/StorageS3Extensions.cs
@@ -0,0 +1,19 @@
+﻿using Amazon.S3;
+
+namespace Api.Extensions;
+
+public static class StorageS3Extensions
+{
+    // Extension helper to ensure bucket exists
+    //public static async Task EnsureBucketExistsAsync(this IAmazonS3 s3, string bucket)
+    public static async void EnsureBucketExistsAsync(this IAmazonS3 s3, string bucket)
+    {
+        //var exists = await s3.DoesS3BucketExistAsync(bucket);
+        await s3.EnsureBucketExistsAsync(bucket);
+
+        //if (!exists)
+        //{
+        //    await s3.PutBucketAsync(new PutBucketRequest { BucketName = bucket });
+        //}
+    }
+}
