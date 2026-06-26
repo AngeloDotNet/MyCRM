bcbf8d7c3729af72d116680c201f693e0054015c
Angelo Pirola <angelo9871@gmail.com>
2026-06-26 09:34:13 +0200

    Add MudBlazor, update packages, set API base URL
    Registered MudBlazor services for UI components, updated HttpClient base address to "http://localhost:5000", upgraded Microsoft.AspNetCore.Components.* packages to 8.0.26, added MudBlazor 9.3.0, and imported relevant namespaces in _Imports.razor.

--- src/WebClient/Program.cs
+++ src/WebClient/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using MyCrm.Client.Services;
 using WebClient;
 using WebClient.Services;
 
@@ -8,8 +10,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// HttpClient to API (assumes API at same host; adjust BaseAddress)
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// HttpClient to API (explicit API URL)
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000") });
+
+// Blazor Storage
+//builder.Services.AddBlazoredLocalStorage();
+
+// MudBlazor
+builder.Services.AddMudServices();
 
 // Token & auth services
 builder.Services.AddScoped<ITokenProvider, TokenProvider>();
